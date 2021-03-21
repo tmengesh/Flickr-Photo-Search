87c566b0ad7d4e440ae4ec96602da557ececb609
Tewodros Mengesha <teddy1496@yahoo.com>
2021-03-21 12:51:55 +0200

    Unit test function to check API response.

--- Flickr Photo Search/Flickr Photo SearchTests/Flickr_Photo_SearchTests.swift
+++ Flickr Photo Search/Flickr Photo SearchTests/Flickr_Photo_SearchTests.swift
@@ -8,7 +8,33 @@
 import XCTest
 @testable import Flickr_Photo_Search
 
+let apiKey = "b59eaa142fbb03d0ba6c93882fd62e30"
+
 class Flickr_Photo_SearchTests: XCTestCase {
+    
+    var apiURL = URL(string: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&text=car&per_page=20&format=json&nojsoncallback=1")
+    
+    func testValidCallToAPIGetsHTTPStatusCode200() throws {
+         
+         let promise = expectation(description: "Status code: 200")
+         let urlSession = URLSession(configuration: .default)
+         
+         let dataTask = urlSession.dataTask(with: apiURL!) { data, response, error in
+             
+             if let error = error {
+               XCTFail("Error: \(error.localizedDescription)")
+               return
+             } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+               if statusCode == 200 {
+                 promise.fulfill()
+               } else {
+                 XCTFail("Status code: \(statusCode)")
+               }
+             }
+           }
+           dataTask.resume()
+           wait(for: [promise], timeout: 5)
+     }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
